795a4b83e0ee54d91464f138d628490f3c72ff71
Mathew <lopezbouma@etml.educanet2.ch>
2020-01-15 13:34:05 +0100

    new sql file

--- choko_db.sql
+++ choko_db.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Client :  localhost
--- Généré le :  Mer 15 Janvier 2020 à 09:35
+-- Généré le :  Mer 15 Janvier 2020 à 12:33
 -- Version du serveur :  5.7.11
 -- Version de PHP :  7.0.3
 
@@ -48,7 +48,7 @@ INSERT INTO `t_category` (`idCategory`, `catName`) VALUES
 CREATE TABLE `t_discussion` (
   `idDiscussion` int(11) NOT NULL,
   `disName` varchar(150) NOT NULL,
-  `idCategory` int(11) NOT NULL,
+  `fkCategory` int(11) NOT NULL,
   `disActive` tinyint(1) NOT NULL DEFAULT '1',
   `disPrioritized` tinyint(1) NOT NULL DEFAULT '0'
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
@@ -57,7 +57,7 @@ CREATE TABLE `t_discussion` (
 -- Contenu de la table `t_discussion`
 --
 
-INSERT INTO `t_discussion` (`idDiscussion`, `disName`, `idCategory`, `disActive`, `disPrioritized`) VALUES
+INSERT INTO `t_discussion` (`idDiscussion`, `disName`, `fkCategory`, `disActive`, `disPrioritized`) VALUES
 (1, 'Minecraft', 1, 1, 0);
 
 -- --------------------------------------------------------
@@ -145,15 +145,15 @@ CREATE TABLE `t_message` (
   `idMessage` int(11) NOT NULL,
   `mesText` text NOT NULL,
   `mesDate` datetime NOT NULL,
-  `idDiscussion` int(11) NOT NULL,
-  `idUser` int(11) NOT NULL
+  `fkDiscussion` int(11) NOT NULL,
+  `fkUser` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Contenu de la table `t_message`
 --
 
-INSERT INTO `t_message` (`idMessage`, `mesText`, `mesDate`, `idDiscussion`, `idUser`) VALUES
+INSERT INTO `t_message` (`idMessage`, `mesText`, `mesDate`, `fkDiscussion`, `fkUser`) VALUES
 (1, 'Bonjour je veux pas etre seul', '2019-11-09 09:53:30', 1, 1);
 
 -- --------------------------------------------------------
@@ -205,24 +205,24 @@ CREATE TABLE `t_user` (
   `idUser` int(11) NOT NULL,
   `usePseudo` varchar(50) NOT NULL,
   `useEmail` varchar(50) NOT NULL,
-  `usePassword` varchar(50) NOT NULL,
+  `usePassword` varchar(100) NOT NULL,
   `useBirthDate` date NOT NULL,
   `useSignUpDate` date NOT NULL,
   `useBio` text NOT NULL,
   `useProfilePicture` varchar(100) NOT NULL,
   `useSignature` varchar(150) NOT NULL,
-  `idGender` int(11) NOT NULL,
-  `idGrade` int(11) NOT NULL,
-  `idRight` int(11) NOT NULL
+  `fkGender` int(11) NOT NULL,
+  `fkGrade` int(11) NOT NULL,
+  `fkRight` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Contenu de la table `t_user`
 --
 
-INSERT INTO `t_user` (`idUser`, `usePseudo`, `useEmail`, `usePassword`, `useBirthDate`, `useSignUpDate`, `useBio`, `useProfilePicture`, `useSignature`, `idGender`, `idGrade`, `idRight`) VALUES
-(1, 'Bakbat', 'aviolat.baptiste@gmail.com', 'chokolaaat', '2000-09-26', '2019-11-09', 'Moi c\'est Baptiste, the sniiiiper', 'chokolaaat/resources/images/timestamp.jpg', 'Membre de la team Panda', 1, 1, 1),
-(2, 'Qosox', 'matheo0204@hotmail.ch', 'chokolaaat', '2001-04-02', '2019-11-09', 'xxxxxxx', 'chokolaaat/resources/images/timestamp.jpg', 'Membre de Chokolaaat', 1, 1, 1);
+INSERT INTO `t_user` (`idUser`, `usePseudo`, `useEmail`, `usePassword`, `useBirthDate`, `useSignUpDate`, `useBio`, `useProfilePicture`, `useSignature`, `fkGender`, `fkGrade`, `fkRight`) VALUES
+(1, 'Bakbat', 'aviolat.baptiste@gmail.com', '$2y$10$FtXQL5v4NEC/8ajxv9EjCeXo7ZWySePX34iBgQD8zyhFvySYeN15.', '2000-09-26', '2019-11-09', 'Moi c\'est Baptiste, the sniiiiper', 'chokolaaat/resources/images/timestamp.jpg', 'Membre de la team Panda', 1, 1, 1),
+(2, 'Qosox', 'matheo0204@hotmail.ch', '$2y$10$FtXQL5v4NEC/8ajxv9EjCeXo7ZWySePX34iBgQD8zyhFvySYeN15.', '2001-04-02', '2019-11-09', 'xxxxxxx', 'chokolaaat/resources/images/timestamp.jpg', 'Membre de Chokolaaat', 1, 1, 1);
 
 --
 -- Index pour les tables exportées
@@ -239,7 +239,7 @@ ALTER TABLE `t_category`
 --
 ALTER TABLE `t_discussion`
   ADD PRIMARY KEY (`idDiscussion`),
-  ADD KEY `t_discussion_t_category_FK` (`idCategory`);
+  ADD KEY `t_discussion_t_category_FK` (`fkCategory`);
 
 --
 -- Index pour la table `t_discussiontag`
@@ -272,8 +272,8 @@ ALTER TABLE `t_like`
 --
 ALTER TABLE `t_message`
   ADD PRIMARY KEY (`idMessage`),
-  ADD KEY `t_message_t_discussion_FK` (`idDiscussion`),
-  ADD KEY `t_message_t_user0_FK` (`idUser`);
+  ADD KEY `t_message_t_discussion_FK` (`fkDiscussion`),
+  ADD KEY `t_message_t_user0_FK` (`fkUser`);
 
 --
 -- Index pour la table `t_right`
@@ -292,9 +292,9 @@ ALTER TABLE `t_tag`
 --
 ALTER TABLE `t_user`
   ADD PRIMARY KEY (`idUser`),
-  ADD KEY `t_user_t_gender_FK` (`idGender`),
-  ADD KEY `t_user_t_grade0_FK` (`idGrade`),
-  ADD KEY `t_user_t_right1_FK` (`idRight`);
+  ADD KEY `t_user_t_gender_FK` (`fkGender`),
+  ADD KEY `t_user_t_grade0_FK` (`fkGrade`),
+  ADD KEY `t_user_t_right1_FK` (`fkRight`);
 
 --
 -- AUTO_INCREMENT pour les tables exportées
@@ -348,7 +348,7 @@ ALTER TABLE `t_user`
 -- Contraintes pour la table `t_discussion`
 --
 ALTER TABLE `t_discussion`
-  ADD CONSTRAINT `t_discussion_t_category_FK` FOREIGN KEY (`idCategory`) REFERENCES `t_category` (`idCategory`);
+  ADD CONSTRAINT `t_discussion_t_category_FK` FOREIGN KEY (`fkCategory`) REFERENCES `t_category` (`idCategory`);
 
 --
 -- Contraintes pour la table `t_discussiontag`
@@ -368,16 +368,16 @@ ALTER TABLE `t_like`
 -- Contraintes pour la table `t_message`
 --
 ALTER TABLE `t_message`
-  ADD CONSTRAINT `t_message_t_discussion_FK` FOREIGN KEY (`idDiscussion`) REFERENCES `t_discussion` (`idDiscussion`),
-  ADD CONSTRAINT `t_message_t_user0_FK` FOREIGN KEY (`idUser`) REFERENCES `t_user` (`idUser`);
+  ADD CONSTRAINT `t_message_t_discussion_FK` FOREIGN KEY (`fkDiscussion`) REFERENCES `t_discussion` (`idDiscussion`),
+  ADD CONSTRAINT `t_message_t_user0_FK` FOREIGN KEY (`fkUser`) REFERENCES `t_user` (`idUser`);
 
 --
 -- Contraintes pour la table `t_user`
 --
 ALTER TABLE `t_user`
-  ADD CONSTRAINT `t_user_t_gender_FK` FOREIGN KEY (`idGender`) REFERENCES `t_gender` (`idGender`),
-  ADD CONSTRAINT `t_user_t_grade0_FK` FOREIGN KEY (`idGrade`) REFERENCES `t_grade` (`idGrade`),
-  ADD CONSTRAINT `t_user_t_right1_FK` FOREIGN KEY (`idRight`) REFERENCES `t_right` (`idRight`);
+  ADD CONSTRAINT `t_user_t_gender_FK` FOREIGN KEY (`fkGender`) REFERENCES `t_gender` (`idGender`),
+  ADD CONSTRAINT `t_user_t_grade0_FK` FOREIGN KEY (`fkGrade`) REFERENCES `t_grade` (`idGrade`),
+  ADD CONSTRAINT `t_user_t_right1_FK` FOREIGN KEY (`fkRight`) REFERENCES `t_right` (`idRight`);
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
